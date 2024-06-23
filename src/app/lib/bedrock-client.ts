9cbc182bbf802cf28ed742f1b758756ec2ab3546
Kian Kasad <kian@kasad.com>
2024-06-22 23:39:10 -0700

    Remove all AWS/Bedrock code

--- src/app/lib/bedrock-client.ts
+++
@@ -1,9 +0,0 @@
-import { BedrockRuntimeClient } from "@aws-sdk/client-bedrock-runtime";
-import { defaultProvider } from "@aws-sdk/credential-provider-node";
-
-const client = new BedrockRuntimeClient({
-  region: "us-east-1", // or your preferred region
-  credentials: defaultProvider(),
-});
-
-export default client;
