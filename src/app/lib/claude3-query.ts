c535c46a9e87153ed27c839528dedc298c3e11bc
Kian Kasad <kian@kasad.com>
2024-06-23 07:00:32 -0700

    Remove API key from code

--- src/app/lib/claude3-query.ts
+++ src/app/lib/claude3-query.ts
@@ -1,12 +1,9 @@
 const Anthropic = require('@anthropic-ai/sdk').default;
 
-const API_KEY = process.env.ANTHROPIC_API_KEY;
-/*const API_KEY = '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';*/
-
 const anthropic = new Anthropic({
-    apiKey: API_KEY,
-    });
-    
+    apiKey: process.env.ANTHROPIC_API_KEY,
+});
+
 export async function analyzeText(text: string): Promise<string> {
     // Construct the prompt for the Anthropic model
     const prompt = `INSTRUCTIONS: "You are an advanced assistant and are well-versed in the policies and ideas of left and right political wings. You will read the following bill and determine where it ranks on a scale from -10 to 10, with any value less than 0 being considered left wing and any value greater than 0 considered right wing. Determine the extent to which the provided text is right or left wing and assign a number to the text in that range. The format will be:
@@ -20,9 +17,9 @@ export async function analyzeText(text: string): Promise<string> {
     try {
         // Call Anthropic API to get analysis
         const msg = await anthropic.messages.create({
-        model: "claude-3-5-sonnet-20240620",
-        max_tokens: 1024,
-        messages: [{ role: "user", content: prompt }],
+            model: "claude-3-5-sonnet-20240620",
+            max_tokens: 1024,
+            messages: [{ role: "user", content: prompt }],
         });
 
         // Extract and return the response
